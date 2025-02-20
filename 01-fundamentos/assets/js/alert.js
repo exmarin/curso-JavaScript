5a1591c78259d6f62937d64153172d0c22ec8a93
exmarin <ex.marinm@gmail.com>
2025-02-20 18:42:09 -0300

    modificacion

--- 01-fundamentos/assets/js/alert.js
+++ 01-fundamentos/assets/js/alert.js
@@ -1,1 +1,14 @@
-alert('Hola Mundo'); // This is a global function that is available in the browser
+// Solicita al usuario que ingrese su nombre y lo almacena en la variable 'nombre'
+let nombre = prompt('¿Cuál es tu nombre?'); 
+
+// Solicita al usuario que ingrese su rol y lo almacena en la variable 'rol'
+let rol = prompt('¿Cuál es tu rol?');
+
+// Genera un número aleatorio entre 1 y 1000 y lo almacena en la variable 'id'
+let id = Math.floor(Math.random() * 1000) + 1;
+
+// Imprime en la consola el nombre y el rol del usuario
+console.log( nombre + ' ' + 'rol: '+ rol  ); // ''
+
+// Imprime en la consola un mensaje de bienvenida que incluye el nombre, el rol y el ID temporal del usuario
+console.log( `Bienvenido/a, ${ nombre }, como ${ rol } a Moodle! Tu ID temporal es: ${ id }` );
--- 01-fundamentos/assets/js/app.js
+++ 01-fundamentos/assets/js/app.js
@@ -1,4 +1,8 @@
+// alert('Hola desde app.js');
 
+
+
+// console.log( console.log('Hola Mundo') );
 
 let a = 10,
     b = 20,
@@ -6,17 +10,15 @@ let a = 10,
     d = 'Spiderman',
     x = a + b;
 
-    const saludo = c + d;
+const saludo =  c + d;
 
-    c = 'Hola de nuevo';
+// console.log('%c Mis variables', 'color:blue; font-weight: bold');
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
 
-    // console.log('%c Mis Variables', 'color:blue; font-weight: bold');
-    // console.log({a});
-    // console.log({b}); 
-    // console.log({c});
+c = 'Hola de nuevo';
 
-    c = 'Hola de nuevo';
-    // console.table({a, b, c, d, x});;
+// console.table({a, b, c, d, x });
 
-    let miNuevoNombre = 'Exequiel Marín';
-    console.log(miNuevoNombre);
+let miNuevoNombre = 'Exequiel Marín';
